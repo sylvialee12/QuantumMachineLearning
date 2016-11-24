fc472f1928e04c0d00a0c5ab782af4ccbe9bc925
LI <xlibb@ust.hk>
2016-11-24 14:38:23 +0800

    Data process including with 2D margin

--- mnist.py
+++ mnist.py
@@ -27,8 +27,7 @@ def data_process(mnist,n):
     shuffle(Data)
     data0=Data[:,0:mnist.data.shape[1]]
     target=Data[:,-1]
-    # psi1=data0/(255.0)
-    # psi2=np.sqrt(1-psi1**2)
+
     psi0=data0/255.0
     psi1=np.cos(np.pi/2*psi0)
     psi2=np.sin(np.pi/2*psi0)
@@ -57,6 +56,30 @@ def data_process(mnist,n):
 
         return data_pooled,lvector
 
+
+@functimer
+def data_process2D(mnist,margin):
+    reshaped_tar=mnist.target.reshape([len(mnist.target),1])
+    Data=np.concatenate((mnist.data, reshaped_tar), axis=1)
+    shuffle(Data)
+    data0=Data[:,0:mnist.data.shape[1]]
+    target=Data[:,-1]
+
+    psi0=data0/255.0
+
+    pixel=int(np.sqrt(data0.shape[1]))
+    psi3=psi0.reshape([data0.shape[0],pixel,pixel])
+    margincol=np.zeros([data0.shape[0],pixel,margin])
+    psi3=np.concatenate((margincol,psi3,margincol),axis=2)
+    marginrow=np.zeros([data0.shape[0],margin,psi3.shape[2]])
+    psi3=np.concatenate((marginrow,psi3,marginrow),axis=1)
+    psi1=np.cos(np.pi/2*psi3).reshape(psi3.shape+(1,))
+    psi2=np.sin(np.pi/2*psi3).reshape(psi3.shape+(1,))
+    data=np.concatenate((psi1,psi2),axis=-1)
+    lvector=np.zeros([target.shape[0],10])
+    for (i,l) in enumerate(lvector):
+        l[int(target[i])]=1
+    return data,lvector
 
 if __name__=="__main__":
     print("yes")
